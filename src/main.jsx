7de9a2d39b41a3c612e8973f94ee39b5442af42e
Kevin Guillermo Arevalo Alveal <kev.arevalo.alveal@gmail.com>
2022-09-25 22:02:28 -0300

    Tarea: Se crea primer componente

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 
+
 //Aqui importamos nuestros componentes
 import { HolaMundo } from './HolaMundo';
+
+//Primera tarea( Crear un componente )
+import { FirstApp } from './FirstApp';
+
 
 ReactDOM.createRoot( document.getElementById('root') ).render(
     <React.StrictMode>
         <HolaMundo />
+        <FirstApp />
     </React.StrictMode>
 )
 
