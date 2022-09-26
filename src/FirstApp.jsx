ecf4ed8e30f735824fcf3b823e9b924e312fef57
Kevin Guillermo Arevalo Alveal <kev.arevalo.alveal@gmail.com>
2022-09-25 22:12:42 -0300

    Uso de fragmentos

--- src/FirstApp.jsx
+++ src/FirstApp.jsx
@@ -1,5 +1,19 @@
+import { Fragment } from "react"
+
+//Uso de fragmentos
+//Se puede ver en los nodos padres: <Fragment></Fragment> o <></>
+//Es un agrupador de otros elementos JSX, siempre deben ir dentro de un nodo padre
+//Pueden haber tantos elementos como queramos dentro del nodo padre
+
 export const FirstApp = () => {
     return (
-        <h1>First App</h1>
+        <>
+            <h1>First App</h1>
+            <p>Probando fragmento</p>
+            <h1>First App</h1>
+            <p>Probando fragmento</p>
+            <h1>First App</h1>
+            <p>Probando fragmento</p>
+        </>
     )
 }
