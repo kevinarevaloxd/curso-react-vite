f02cf747933b15cac809e8ceb6552eaf72ef91cd
Kevin Guillermo Arevalo Alveal <kev.arevalo.alveal@gmail.com>
2022-09-25 21:35:25 -0300

    Hola Mundo en React

---
+++ src/main.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+
+function App() {
+    //document.createElement...(Otra forma de )
+    return (<h1>Hola Mundo</h1>)
+}
+
+ReactDOM.createRoot( document.getElementById('root') ).render(
+    <React.StrictMode>
+        <App />
+    </React.StrictMode>
+
+)
