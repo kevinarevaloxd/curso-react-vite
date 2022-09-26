db712f07c0b25e9765027396aea14cdc7f91597e
Kevin Guillermo Arevalo Alveal <kev.arevalo.alveal@gmail.com>
2022-09-25 22:42:50 -0300

    Implementado estilos con CSS

--- src/main.jsx
+++ src/main.jsx
@@ -5,8 +5,9 @@ import ReactDOM from 'react-dom/client';
 //Aqui importamos nuestros componentes
 import { HolaMundo } from './HolaMundo';
 
-//Primera tarea( Crear un componente )
-import { FirstApp } from './FirstApp';
+import { FirstApp } from './FirstApp'; //Primera tarea( Crear un componente )
+
+import './styles.css'; //Implementando estilos con CSS
 
 
 ReactDOM.createRoot( document.getElementById('root') ).render(
