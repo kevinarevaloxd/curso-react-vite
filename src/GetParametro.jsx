3c78e74423715311dd273c3e62d85a068ce9113f
Kevin Guillermo Arevalo Alveal <kev.arevalo.alveal@gmail.com>
2022-09-26 12:15:17 -0300

    PORTAFOLIO: Se obtienen parametros dinamicos de la URL

--- src/GetParametro.jsx
+++ src/GetParametro.jsx
@@ -1,39 +1,41 @@
 import axios from "axios";
 import React from "react";
-import { createSearchParams, useNavigate, useSearchParams } from "react-router-dom";
+import { useSearchParams } from "react-router-dom";
 
-
-export const Detail = (props) => {
+export const Detail = async(props) => {
   const [searchParams] = useSearchParams();
-  let token_ws = searchParams.get('token_ws')
-  let TBK_TOKEN = searchParams.get('TBK_TOKEN')
-  let TBK_ORDEN_COMPRA = searchParams.get('TBK_ORDEN_COMPRA')
-  let TBK_ID_SESION = searchParams.get('TBK_ID_SESION')
-  const params = [];
-  for(let entry of searchParams.entries()) {
-    params.push(entry);
-  }
+  const url = 'http://localhost:9090/api/reservas/transaccion?'
+  let query = searchParams.toString();
+  await fetch(url + query).then(resp => {
+    resp.json();
+  })
+
+
+  // const params = [];
+  // for(let entry of searchParams.entries()) {
+  //   params.push(entry);
+  // }
+  // console.log(params.toString())
+  // // let query = Object.keys(params).map(k => `${esc(k)}=${esc(params[k])}`).join('&')
+  // let query = Object.keys(params).map(k => `${(k)}=${(params[k])}`).join('&')
+  // console.log(query)
   return (
     <>
-      <ul>
+      {/* <ul>
         {params.map(([key, value]) => (
           <li key={key}>{key} - {value}</li>
         ))}
-      </ul>
-      <div>
-        <form action="http://localhost:9090/api/reservas/transaccion" method="GET">
-            <input type="hidden" name="token_ws" value={token_ws}/>
-            <input type="hidden" name="TBK_TOKEN" value={TBK_TOKEN}/>
-            <input type="hidden" name="TBK_TOKEN" value={TBK_ORDEN_COMPRA}/>
-            <input type="hidden" name="TBK_ID_SESION" value={TBK_ID_SESION}/>
-            <button type="submit">GET</button>
-        </form>
-      </div>
+      </ul> */}
+      <div>{url}</div>
     </>
   );
 };
 
 export const ObtenerParametro = () => {
+  const url = 'http://localhost:9090/api/reservas/transaccion?'
+  // fetch(url + query).then(resp => {
+  //   resp.json();
+  // })
   return (
     <>
       <h1>Obtener el parametro</h1>
