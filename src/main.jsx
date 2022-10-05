56a085acc2e3576ff3058d70c0f6dc2a62087ca3
Kevin Guillermo Arevalo Alveal <kev.arevalo.alveal@gmail.com>
2022-10-04 22:25:23 -0300

    Uso del primer Hook useState

--- src/main.jsx
+++ src/main.jsx
@@ -1,23 +1,24 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
+import React from "react";
+import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 
-
 //Aqui importamos nuestros componentes
-import { HolaMundo } from './HolaMundo';
-import { FirstApp } from './FirstApp'; //Primera tarea( Crear un componente )
-import { ObtenerParametro, Detail } from './GetParametro';
-import { FormArray } from './FormArray';
-import { SubirArchivo } from './SubirArchivo';
-import './styles.css'; //Implementando estilos con CSS
+// import { HolaMundo } from './HolaMundo';
+// import { FirstApp } from './FirstApp'; //Primera tarea( Crear un componente )
+// import { ObtenerParametro, Detail } from './GetParametro';
+// import { FormArray } from './FormArray';
+// import { SubirArchivo } from './SubirArchivo';
+import { CounterApp } from "./CounterApp";
+import "./styles.css"; //Implementando estilos con CSS
 
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 
 root.render(
-        <BrowserRouter>
-        <FormArray />
-        </BrowserRouter>
-    );
-
-
+  <React.StrictMode>
+    <CounterApp value={20} />
+    {/* <FirstApp title="Hola, Soy Vegeta" /> */}
+  </React.StrictMode>
+  // <BrowserRouter>
+  // <CounterApp value={10} />
+  // </BrowserRouter>
+);
